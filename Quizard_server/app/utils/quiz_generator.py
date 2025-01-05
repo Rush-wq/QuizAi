49d4e0e2231df4a2f75b53ca6d3e19a693eed13d
Ethan Rush <rushsoftwaredev@gmail.com>
2025-01-05 00:16:26 -0500

    Fixing file issues

--- Quizard_server/app/utils/quiz_generator.py
+++ Quizard_server/app/utils/quiz_generator.py
@@ -13,14 +13,14 @@ class QuizGenerator(object):
     def __init__(self):
         default_path = os.path.dirname(os.path.abspath(__file__))
 
-        with open(os.path.join(default_path, 'prompt_tools\prompt_start.txt'), "r") as file:
-            self.prompt_start_text  = file.read()
+        with open(os.path.join(default_path, 'prompt_tools', 'prompt_start.txt'), "r") as file:
+            self.prompt_start_text = file.read()
 
-        with open(os.path.join(default_path, 'prompt_tools\prompt_end.txt'), "r") as file:
-            self.prompt_end_text  = file.read()
+        with open(os.path.join(default_path, 'prompt_tools', 'prompt_end.txt'), "r") as file:
+            self.prompt_end_text = file.read()
 
-        with open(os.path.join(default_path, 'prompt_tools\prompt_json_schema.txt'), "r") as file:
-            self.prompt_josn_schema_text  = file.read()
+        with open(os.path.join(default_path, 'prompt_tools', 'prompt_json_schema.txt'), "r") as file:
+            self.prompt_josn_schema_text = file.read()
 
         googleGenai.configure(api_key=os.environ["GOOGLE_API_KEY"])
         self.model = googleGenai.GenerativeModel("gemini-2.0-flash-exp") 
